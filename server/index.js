357b0e2ecbf6e4541dbfb55e6c1e70d523c96d88
David Diner <david.diner@wework.com>
2017-11-22 17:17:54 -0500

    Add privacy policy.

--- server/index.js
+++ server/index.js
@@ -9,11 +9,12 @@ const port = (process.env.PORT || 8080)
 const app = express()
 
 const indexPath = path.join(__dirname, '../dist/index.html')
+const privacyPath = path.join(__dirname, '../dist/privacy.html')
 const publicPath = express.static(path.join(__dirname, '../dist'))
 
 app.use(publicPath)
 app.get('/', function (_, res) { res.sendFile(indexPath) })
-app.get('/.well-known/acme-challenge/Pw949ZGbMG6jqmrxl7NY2OzbZdjikRekTL61BpIKWeM', function (_, res) { res.send('Pw949ZGbMG6jqmrxl7NY2OzbZdjikRekTL61BpIKWeM.oDxjYQTlNZeMmPaxPcCS0vfu0bq8ea2SWxBqahXFsbM') })
+app.get('/privacy', function (_, res) { res.sendFile(privacyPath) })
 
 app.listen(port)
 console.log(`Listening at http://localhost:${port}`)
--- webpack.common.js
+++ webpack.common.js
@@ -1,36 +1,40 @@
 const path = require('path');
-
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
 const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
   template: './app/index.html',
   filename: 'index.html',
-  inject: 'body'
-})
-const CleanWebpackPlugin =require('clean-webpack-plugin');
+  inject: 'body',
+});
+const HtmlWebpackPluginPrivacy = new HtmlWebpackPlugin({
+  filename: 'privacy.html',
+  template: './app/privacy.html',
+});
 const CleanWebpackPluginConfig = new CleanWebpackPlugin(['dist']);
 
 module.exports = {
   entry: './app/index.js',
   output: {
     path: path.resolve('dist'),
-    filename: 'index_bundle.js'
+    filename: 'index_bundle.js',
   },
   resolve: {
-    extensions: ['.js', '.jsx']
+    extensions: ['.js', '.jsx'],
   },
   module: {
     rules: [
       {
         test: /\.jsx?$/,
         loader: 'babel-loader',
-        exclude: /node_modules/
+        exclude: /node_modules/,
       },
 
       {
         test: /\.scss/,
-        use: ['style-loader', 'css-loader', 'sass-loader']
-      }
-    ]
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+    ],
   },
-  plugins: [CleanWebpackPluginConfig, HtmlWebpackPluginConfig]
-}
+  plugins: [CleanWebpackPluginConfig, HtmlWebpackPluginConfig, HtmlWebpackPluginPrivacy],
+};
